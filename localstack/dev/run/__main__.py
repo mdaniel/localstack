417246f09c5f6b5d4379e7bc118c4edf73246055
Thomas Rausch <thomas@thrau.at>
2023-08-15 21:57:18 +0200

    split up code, add wip features

--- localstack/dev/run/__main__.py
+++ localstack/dev/run/__main__.py
@@ -1,181 +1,21 @@
-import os
-import pty
-import select
-import shlex
-import subprocess
-import sys
-import termios
-import tty
-from typing import Optional
+from typing import Tuple
 
 import click
 from rich.rule import Rule
 
-from localstack import config, constants
+from localstack import config
 from localstack.cli import console
-from localstack.utils.bootstrap import get_docker_image_to_start
-from localstack.utils.container_utils.container_client import (
-    ContainerConfiguration,
-    PortMappings,
-    VolumeBind,
-    VolumeMappings,
-)
+from localstack.utils.container_utils.container_client import ContainerConfiguration
 from localstack.utils.container_utils.docker_cmd_client import CmdDockerClient
+from localstack.utils.run import run_interactive
+from localstack.utils.strings import short_uid
 
-
-def get_container_config():
-    return ContainerConfiguration(
-        image_name=get_docker_image_to_start(),
-        name=config.MAIN_CONTAINER_NAME + "_dev",
-        volumes=VolumeMappings(),
-        remove=True,
-        # FIXME: update with https://github.com/localstack/localstack/pull/7991
-        ports=PortMappings(bind_host=config.EDGE_BIND_HOST),
-        entrypoint=os.environ.get("ENTRYPOINT"),
-        command=shlex.split(os.environ.get("CMD", "")) or None,
-        env_vars={},
-    )
-
-
-def run_interactive(command: list[str]):
-    """Run an interactive command in a subprocess. Copied from https://stackoverflow.com/a/43012138/804840"""
-    # save original tty setting then set it to raw mode
-    old_tty = termios.tcgetattr(sys.stdin)
-    tty.setraw(sys.stdin.fileno())
-
-    # open pseudo-terminal to interact with subprocess
-    master_fd, slave_fd = pty.openpty()
-
-    try:
-        # use os.setsid() make it run in a new process group, or bash job control will not be enabled
-        p = subprocess.Popen(
-            command,
-            preexec_fn=os.setsid,
-            stdin=slave_fd,
-            stdout=slave_fd,
-            stderr=slave_fd,
-            universal_newlines=True,
-        )
-
-        while p.poll() is None:
-            r, w, e = select.select([sys.stdin, master_fd], [], [])
-            if sys.stdin in r:
-                d = os.read(sys.stdin.fileno(), 10240)
-                os.write(master_fd, d)
-                if d == b"\x04":
-                    break
-            elif master_fd in r:
-                o = os.read(master_fd, 10240)
-                if o:
-                    os.write(sys.stdout.fileno(), o)
-    finally:
-        # restore tty settings back
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_tty)
-
-
-class DevContainerConfigurator:
-    site_packages_target_dir = "/opt/code/localstack/.venv/lib/python3.10/site-packages"
-
-    localstack_project_dir: str
-    localstack_ext_project_dir: str
-    venv_path: str
-    env: dict[str, str]
-    pro: bool
-    mount_source: bool
-    image_name: Optional[str]
-
-    def __init__(
-        self,
-        *,
-        workspace_dir: str = None,
-        localstack_project_dir: str = None,
-        localstack_ext_project_dir: str = None,
-        venv_path: str = None,
-        image_name: str = None,
-        volume_dir: str = None,
-        env: dict[str, str] = None,
-        pro: bool = False,
-        mount_source: bool = True,
-        mount_entrypoints: bool = True,
-    ):
-        self.workspace_dir = workspace_dir or os.path.abspath(os.path.join(os.getcwd(), ".."))
-
-        self.localstack_project_dir = localstack_project_dir or os.path.join(
-            self.workspace_dir, "localstack"
-        )
-        self.localstack_ext_project_dir = localstack_ext_project_dir or os.path.join(
-            self.workspace_dir, "localstack-ext"
-        )
-
-        self.venv_path = venv_path or os.path.join(os.getcwd(), ".venv")
-
-        self.volume_dir = volume_dir or "/tmp/localstack"
-        self.env = env or {}
-        self.pro = pro
-        self.image_name = image_name
-        self.mount_source = mount_source
-        self.mount_entrypoints = mount_entrypoints
-
-    def configure(self, cfg: ContainerConfiguration):
-        # configure volume mounts
-        if not cfg.volumes:
-            cfg.volumes = VolumeMappings()
-        if not cfg.ports:
-            cfg.ports = PortMappings()
-
-        # basic volume directory
-        if not self.volume_dir:
-            raise ValueError("specify a volume directory")
-
-        cfg.volumes.append(VolumeBind(self.volume_dir, constants.DEFAULT_VOLUME_DIR))
-
-        # environment variables
-        if self.pro:
-            from localstack_ext import config as config_ext  # noqa
-        for env_var in config.CONFIG_ENV_VARS:
-            value = os.environ.get(env_var, None)
-            if value is not None:
-                cfg.env_vars[env_var] = value
-
-        # ports
-        # TODO: allow randomization (should the configurator even do this?)
-        cfg.ports.add(4566)
-        cfg.ports.add([4510, 4599])
-
-        if self.pro:
-            # overwrite pro config
-            self._configure_pro(cfg)
-        else:
-            # community-specific config (some source directories are different)
-            self._configure_community(cfg)
-
-        if self.image_name:
-            cfg.image_name = self.image_name
-
-    def _configure_community(self, cfg: ContainerConfiguration):
-        cfg.image_name = constants.DOCKER_IMAGE_NAME
-
-        if self.mount_source:
-            source = os.path.join(self.localstack_project_dir, "localstack")
-            if os.path.exists(source):
-                cfg.volumes.add(VolumeBind(source, "/opt/code/localstack/localstack"))
-
-    def _configure_pro(self, cfg: ContainerConfiguration):
-        cfg.image_name = constants.DOCKER_IMAGE_NAME_PRO
-
-        # mount source code folders
-        if self.mount_source:
-            # in the pro container, localstack core is installed into the site packages
-            source = os.path.join(self.localstack_project_dir, "localstack")
-            if os.path.exists(source):
-                target = os.path.join(self.site_packages_target_dir, "localstack")
-                cfg.volumes.add(VolumeBind(source, target))
-
-            # localstack_ext source files
-            source = os.path.join(self.localstack_ext_project_dir, "localstack_ext")
-            if os.path.exists(source):
-                target = os.path.join(self.site_packages_target_dir, "localstack_ext")
-                cfg.volumes.add(VolumeBind(source, target))
+from .configurators import (
+    ConfigEnvironmentConfigurator,
+    ImageConfigurator,
+    PortConfigurator,
+    VolumeMountConfigurator,
+)
 
 
 @click.command("run")
@@ -184,7 +24,15 @@ class DevContainerConfigurator:
 @click.option("--pro", is_flag=True, default=False)
 @click.option("--randomize", is_flag=True, default=False)
 @click.option("--mount-source/--no-mount-source", is_flag=True, default=True)
+@click.option("--mount-dependencies/--no-mount-dependencies", is_flag=True, default=False)
 @click.option("--mount-entrypoints/--no-mount-entrypoints", is_flag=True, default=False)
+@click.option(
+    "--env",
+    "-e",
+    help="Additional environment variables that are passed to the LocalStack container",
+    multiple=True,
+    required=False,
+)
 @click.argument("command", nargs=-1, required=False)
 def run(
     image: str = None,
@@ -192,7 +40,9 @@ def run(
     pro: bool = False,
     randomize: bool = False,
     mount_source: bool = True,
+    mount_dependencies: bool = False,
     mount_entrypoints: bool = False,
+    env: Tuple = (),
     command: str = None,
 ):
     console.print(locals())
@@ -204,43 +54,49 @@ def run(
         entrypoint = None
         command = None
 
-    docker = CmdDockerClient()
+    status = console.status("Configuring")
+    status.start()
 
-    cfg = ContainerConfiguration(
+    # setup configuration
+    container_config = ContainerConfiguration(
         image_name=image,
-        name=config.MAIN_CONTAINER_NAME,
-        volumes=VolumeMappings(),
+        name=config.MAIN_CONTAINER_NAME
+        if not randomize
+        else config.MAIN_CONTAINER_NAME + "_" + short_uid(),
         remove=True,
-        # FIXME: update with https://github.com/localstack/localstack/pull/7991
-        ports=PortMappings(bind_host=config.EDGE_BIND_HOST),
         entrypoint=entrypoint,
         interactive=True,
         tty=True,
         command=command,
-        env_vars={},
     )
+    configurators = [
+        PortConfigurator(randomize=False),
+        ImageConfigurator(pro, image),
+        ConfigEnvironmentConfigurator(pro, env),
+        VolumeMountConfigurator(
+            volume_dir=volume_dir,
+            pro=pro,
+            mount_source=mount_source,
+            mount_dependencies=mount_dependencies,
+            mount_entrypoints=mount_entrypoints,
+        ),
+    ]
+    for configurator in configurators:
+        configurator(container_config)
+    console.print(container_config.__dict__)
 
-    dev_config = DevContainerConfigurator(
-        image_name=image,
-        volume_dir=volume_dir,
-        pro=pro,
-        # randomize=randomize,
-        mount_source=mount_source,
-        mount_entrypoints=mount_entrypoints,
-    )
-
-    dev_config.configure(cfg)
-    console.print(cfg.__dict__)
-
-    container_id = docker.create_container_from_config(cfg)
-
+    # run the container
+    docker = CmdDockerClient()
+    status.update("Creating container")
+    container_id = docker.create_container_from_config(container_config)
+    status.stop()
     rule = Rule("Interactive session 💻")
     console.print(rule)
     try:
         cmd = [*docker._docker_cmd(), "start", "--interactive", "--attach", container_id]
         run_interactive(cmd)
     finally:
-        if cfg.remove:
+        if container_config.remove:
             try:
                 if docker.is_container_running(container_id):
                     docker.stop_container(container_id)
---
+++ localstack/dev/run/configurators.py
@@ -0,0 +1,175 @@
+import os
+from typing import Tuple
+
+from localstack import config, constants
+from localstack.utils.container_utils.container_client import (
+    ContainerConfiguration,
+    PortMappings,
+    VolumeBind,
+    VolumeMappings,
+)
+from localstack.utils.docker_utils import DOCKER_CLIENT
+from localstack.utils.files import get_user_cache_dir
+from localstack.utils.run import run
+
+
+class VolumeMountConfigurator:
+    site_packages_target_dir = "/opt/code/localstack/.venv/lib/python3.10/site-packages"
+
+    localstack_project_dir: str
+    localstack_ext_project_dir: str
+    venv_path: str
+    pro: bool
+    mount_source: bool
+    mount_dependencies: bool
+    mount_entrypoints: bool
+
+    def __init__(
+        self,
+        *,
+        workspace_dir: str = None,
+        localstack_project_dir: str = None,
+        localstack_ext_project_dir: str = None,
+        venv_path: str = None,
+        volume_dir: str = None,
+        pro: bool = False,
+        mount_source: bool = True,
+        mount_dependencies: bool = False,
+        mount_entrypoints: bool = False,
+    ):
+        self.workspace_dir = workspace_dir or os.path.abspath(os.path.join(os.getcwd(), ".."))
+
+        self.localstack_project_dir = localstack_project_dir or os.path.join(
+            self.workspace_dir, "localstack"
+        )
+        self.localstack_ext_project_dir = localstack_ext_project_dir or os.path.join(
+            self.workspace_dir, "localstack-ext"
+        )
+        self.venv_path = venv_path or os.path.join(os.getcwd(), ".venv")
+        self.volume_dir = volume_dir or "/tmp/localstack"
+
+        self.pro = pro
+        self.mount_source = mount_source
+        self.mount_dependencies = mount_dependencies
+        self.mount_entrypoints = mount_entrypoints
+        self.container_client = DOCKER_CLIENT
+
+    def list_files_in_image(self, cfg: ContainerConfiguration) -> list[str]:
+        if not cfg.image_name:
+            raise ValueError("missing image name")
+
+        image_id = self.container_client.inspect_image(cfg.image_name)["Id"]
+
+        cache_dir = get_user_cache_dir() / "localstack-dev-cli"
+        cache_dir.mkdir(exist_ok=True, parents=True)
+        cache_file = cache_dir / f"{image_id}.files.txt"
+
+        if cache_file.exists():
+            files = cache_file.read_text().splitlines(keepends=False)
+        else:
+            container_id = self.container_client.create_container(image_name=cfg.image_name)
+            try:
+                out = run(f"docker export {container_id} | tar -t", shell=True)
+                cache_file.write_text(out)
+                files = out.splitlines(keepends=False)
+            finally:
+                self.container_client.remove_container(container_id)
+
+        return files
+
+    def __call__(self, cfg: ContainerConfiguration):
+        # first inspect the image file system
+        if self.mount_entrypoints or self.mount_dependencies:
+            raise NotImplementedError
+            # TODO:
+            # for file in self.list_files_in_image(cfg):
+            #     if file.startswith("opt/code/localstack/.venv/"):
+            #         if file.endswith("entry_points.txt"):
+            #             print(file)
+
+        # configure volume mounts
+        if not cfg.volumes:
+            cfg.volumes = VolumeMappings()
+
+        # basic volume directory
+        if not self.volume_dir:
+            raise ValueError("specify a volume directory")
+
+        cfg.volumes.append(VolumeBind(self.volume_dir, constants.DEFAULT_VOLUME_DIR))
+
+        if self.pro:
+            # overwrite pro config
+            self._configure_pro(cfg.volumes)
+        else:
+            # community-specific config (some source directories are different)
+            self._configure_community(cfg.volumes)
+
+    def _configure_community(self, volumes: VolumeMappings):
+        if self.mount_source:
+            source = os.path.join(self.localstack_project_dir, "localstack")
+            if os.path.exists(source):
+                volumes.add(VolumeBind(source, "/opt/code/localstack/localstack", read_only=True))
+
+    def _configure_pro(self, volumes: VolumeMappings):
+        # mount source code folders
+        if self.mount_source:
+            # in the pro container, localstack core is installed into the site packages
+            source = os.path.join(self.localstack_project_dir, "localstack")
+            if os.path.exists(source):
+                target = os.path.join(self.site_packages_target_dir, "localstack")
+                volumes.add(VolumeBind(source, target, read_only=True))
+
+            # localstack_ext source files
+            source = os.path.join(self.localstack_ext_project_dir, "localstack_ext")
+            if os.path.exists(source):
+                target = os.path.join(self.site_packages_target_dir, "localstack_ext")
+                volumes.add(VolumeBind(source, target, read_only=True))
+
+
+class PortConfigurator:
+    def __init__(self, randomize: bool = True):
+        self.randomize = randomize
+
+    def __call__(self, cfg: ContainerConfiguration):
+        if not cfg.ports:
+            cfg.ports = PortMappings(bind_host=config.EDGE_BIND_HOST)
+
+        if self.randomize:
+            # TODO: randomize ports and set config accordingly (also set GATEWAY_LISTEN!)
+            raise NotImplementedError
+        else:
+            cfg.ports.add(4566)
+            cfg.ports.add([config.EXTERNAL_SERVICE_PORTS_START, config.EXTERNAL_SERVICE_PORTS_END])
+
+
+class ImageConfigurator:
+    def __init__(self, pro: bool, image_name: str | None):
+        self.pro = pro
+        self.image_name = image_name
+
+    def __call__(self, cfg: ContainerConfiguration):
+        if self.image_name:
+            cfg.image_name = self.image_name
+        else:
+            if self.pro:
+                cfg.image_name = constants.DOCKER_IMAGE_NAME_PRO
+            else:
+                cfg.image_name = constants.DOCKER_IMAGE_NAME
+
+
+class ConfigEnvironmentConfigurator:
+    def __init__(self, pro: bool, env_args: Tuple[str] = ()):
+        self.pro = pro
+        self.env_args = env_args
+
+    def __call__(self, cfg: ContainerConfiguration):
+        if self.pro:
+            from localstack_ext import config as config_ext  # noqa
+
+        if cfg.env_vars is None:
+            cfg.env_vars = {}
+
+        for env_var in config.CONFIG_ENV_VARS:
+            value = os.environ.get(env_var, None)
+            if value is not None:
+                cfg.env_vars[env_var] = value
